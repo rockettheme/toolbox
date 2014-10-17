f0e8b8ddc8b2478b919171b6bf12d90c39884b11
Matias Griese <matias@kunena.org>
2014-10-17 09:41:42 +0300

    Some improvements to StreamBuilder class

--- StreamWrapper/src/StreamBuilder.php
+++ StreamWrapper/src/StreamBuilder.php
@@ -1,8 +1,15 @@
 <?php
 namespace RocketTheme\Toolbox\StreamWrapper;
 
+/**
+ * Class StreamBuilder
+ * @package RocketTheme\Toolbox\StreamWrapper
+ */
 class StreamBuilder
 {
+    /**
+     * @var array
+     */
     protected $items = [];
 
     public function __construct(array $items = [])
@@ -12,43 +19,69 @@ class StreamBuilder
         }
     }
 
+    /**
+     * @param $scheme
+     * @param $handler
+     * @return $this
+     * @throws \InvalidArgumentException
+     */
     public function add($scheme, $handler)
     {
         if (isset($this->items[$scheme])) {
             if ($handler == $this->items[$scheme]) {
-                return;
+                return $this;
             }
-            throw new \InvalidArgumentException("Stream '{$handler}' has already been initialized.");
+            throw new \InvalidArgumentException("Stream '{$scheme}' has already been initialized.");
         }
 
-        if (!is_a('StreamInterface', $handler, true)) {
-            throw new \InvalidArgumentException("Stream '{$handler}' has unknown or invalid type.");
+        if (!is_subclass_of($handler, 'RocketTheme\Toolbox\StreamWrapper\StreamInterface')) {
+            throw new \InvalidArgumentException("Stream '{$scheme}' has unknown or invalid type.");
         }
 
-        if (!stream_wrapper_register($scheme, $handler)) {
-            throw new \InvalidArgumentException("Stream '{$handler}' could not be initialized.");
+        if (!@stream_wrapper_register($scheme, $handler)) {
+            throw new \InvalidArgumentException("Stream '{$scheme}' could not be initialized.");
         }
+
         $this->items[$scheme] = $handler;
+
+        return $this;
     }
 
+    /**
+     * @param $scheme
+     * @return $this
+     */
     public function remove($scheme)
     {
         if (isset($this->items[$scheme])) {
             stream_wrapper_unregister($scheme);
             unset($this->items[$scheme]);
         }
+
+        return $this;
     }
 
+    /**
+     * @return array
+     */
     public function getStreams()
     {
         return $this->items;
     }
 
+    /**
+     * @param $scheme
+     * @return bool
+     */
     public function isStream($scheme)
     {
         return isset($this->items[$scheme]);
     }
 
+    /**
+     * @param $scheme
+     * @return null
+     */
     public function getStreamType($scheme)
     {
         return isset($this->items[$scheme]) ? $this->items[$scheme] : null;
