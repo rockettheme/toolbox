174d69d4213bfa9a2b6d84312b918a372e65f5da
Nicolas Potier <nicolas.potier@acseo-conseil.fr>
2016-10-05 17:25:54 +0200

    Remove support for PHP version <= 5.4

--- Session/src/Session.php
+++ Session/src/Session.php
@@ -29,11 +29,7 @@ class Session implements \IteratorAggregate
     private function isSessionStarted()
     {
         if ( php_sapi_name() !== 'cli' ) {
-            if ( version_compare(phpversion(), '5.4.0', '>=') ) {
-                return session_status() == PHP_SESSION_ACTIVE ? TRUE : FALSE;
-            } else {
-                return !(session_id === '');
-            }
+            return !(session_id === '');
         }
     }
     
