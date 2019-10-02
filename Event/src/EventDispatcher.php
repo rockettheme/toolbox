2a90a5af58483dcc47f942d4ed67d0796a60acf7
Jack Thomas <Jack.Thomas@elavon.com>
2019-10-02 16:15:20 -0400

    Fixed method signature

--- Event/src/EventDispatcher.php
+++ Event/src/EventDispatcher.php
@@ -14,11 +14,13 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface;
  */
 class EventDispatcher extends BaseEventDispatcher implements EventDispatcherInterface
 {
-    public function dispatch($event = null, $eventName)
+    public function dispatch($event/*, string $eventName = null*/)
     {
         if (null === $event) {
             $event = new Event();
         }
+
+        $eventName = 1 < \func_num_args() ? func_get_arg(1) : null;
 
         return parent::dispatch($event, $eventName);
     }
