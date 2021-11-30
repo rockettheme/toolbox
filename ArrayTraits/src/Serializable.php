eb176d6c89d54e4fb82bc082234323320df07746
Matias Griese <matias@trilbymedia.com>
2021-11-30 21:13:07 +0200

    If you use `ArrayTraits\Seriazable`, make sure you do not override the methods (use PHP 8 methods instead)

--- ArrayTraits/src/Serializable.php
+++ ArrayTraits/src/Serializable.php
@@ -15,11 +15,13 @@ trait Serializable
      * Returns string representation of the object.
      *
      * @return string  Returns the string representation of the object.
+     * @final
+     * @deprecated Override `__serialize()` instead.
      */
     #[\ReturnTypeWillChange]
     public function serialize()
     {
-        return serialize($this->items);
+        return serialize($this->__serialize());
     }
 
     /**
@@ -27,10 +29,31 @@ trait Serializable
      *
      * @param string $serialized  The string representation of the object.
      * @return void
+     * @final
+     * @deprecated Override `__unserialize()` instead.
      */
     #[\ReturnTypeWillChange]
     public function unserialize($serialized)
     {
-        $this->items = unserialize($serialized);
+        $this->__unserialize(unserialize($serialized));
+    }
+
+    /**
+     * @return array
+     */
+    #[\ReturnTypeWillChange]
+    public function __serialize()
+    {
+        return $this->items;
+    }
+
+    /**
+     * @param array $data
+     * @return void
+     */
+    #[\ReturnTypeWillChange]
+    public function __unserialize($data)
+    {
+        $this->items = $data;
     }
 }
