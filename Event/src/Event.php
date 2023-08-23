179ed57a3b896932394952c7be74066b25672882
Mihael Kvar <miha@lxnav.com>
2023-08-23 14:56:46 +0200

    -Completely removed dependency on symfony/event (due to incompatibility with our setup). Thus this package is no longer can no longer be used where symfony/event is required, but is otherwise functionally the same.

--- Event/src/Event.php
+++ Event/src/Event.php
@@ -5,20 +5,65 @@ namespace RocketTheme\Toolbox\Event;
 use RocketTheme\Toolbox\ArrayTraits\ArrayAccess;
 use RocketTheme\Toolbox\ArrayTraits\Constructor;
 use RocketTheme\Toolbox\ArrayTraits\Export;
-use Symfony\Component\EventDispatcher\Event as BaseEvent;
 
-/**
- * Implements Symfony Event interface.
- *
- * @package RocketTheme\Toolbox\Event
- * @author RocketTheme
- * @license MIT
- * @deprecated Event classes will be removed in the future. Use PSR-14 implementation instead.
- */
-class Event extends BaseEvent implements \ArrayAccess
-{
-    use ArrayAccess, Constructor, Export;
 
-    /** @var array */
-    protected $items = [];
+use Psr\EventDispatcher\StoppableEventInterface;
+
+if (interface_exists(StoppableEventInterface::class)) {
+    /**
+     * Implements Symfony Event interface.
+     *
+     * @package RocketTheme\Toolbox\Event
+     * @author RocketTheme
+     * @license MIT
+     */
+    class Event implements \ArrayAccess
+    {
+        use ArrayAccess, Constructor, Export;
+
+        private $propagationStopped = false;
+
+        /** @var array */
+        protected $items = [];
+
+        public function isPropagationStopped()
+        {
+            return $this->propagationStopped;
+        }
+
+        public function stopPropagation()
+        {
+            $this->propagationStopped = true;
+        }
+    }
+} else {
+    /**
+     * Implements Symfony Event interface.
+     *
+     * @package RocketTheme\Toolbox\Event
+     * @author RocketTheme
+     * @license MIT
+     */
+    class Event implements \ArrayAccess
+    {
+        use ArrayAccess, Constructor, Export;
+
+        private $propagationStopped = false;
+
+        /** @var array */
+        protected $items = [];
+
+        /**
+         * @return bool Whether propagation was already stopped for this event
+         */
+        public function isPropagationStopped()
+        {
+            return $this->propagationStopped;
+        }
+
+        public function stopPropagation()
+        {
+            $this->propagationStopped = true;
+        }
+    }
 }
--- Event/src/EventSubscriberInterface.php
+++ Event/src/EventSubscriberInterface.php
@@ -2,16 +2,35 @@
 
 namespace RocketTheme\Toolbox\Event;
 
-use Symfony\Component\EventDispatcher\EventSubscriberInterface as BaseEventSubscriberInterface;
-
 /**
  * Defines EventSubscriberInterface.
  *
  * @package RocketTheme\Toolbox\Event
  * @author RocketTheme
  * @license MIT
- * @deprecated Event classes will be removed in the future. Use PSR-14 implementation instead.
  */
-interface EventSubscriberInterface extends BaseEventSubscriberInterface
+interface EventSubscriberInterface
 {
+  /**
+   * Returns an array of event names this subscriber wants to listen to.
+   *
+   * The array keys are event names and the value can be:
+   *
+   *  * The method name to call (priority defaults to 0)
+   *  * An array composed of the method name to call and the priority
+   *  * An array of arrays composed of the method names to call and respective
+   *    priorities, or 0 if unset
+   *
+   * For instance:
+   *
+   *  * ['eventName' => 'methodName']
+   *  * ['eventName' => ['methodName', $priority]]
+   *  * ['eventName' => [['methodName1', $priority], ['methodName2']]]
+   *
+   * The code must not depend on runtime state as it will only be called at compile time.
+   * All logic depending on runtime state must be put into the individual methods handling the events.
+   *
+   * @return array<string, mixed> The event names to listen to
+   */
+  public static function getSubscribedEvents();
 }
